253dea58abd72270ee78d24642c792f1f338e931
DariFSG <119135633+DariFSG@users.noreply.github.com>
2023-04-26 18:26:37 +0300

    Add files via upload

---
+++ 3LL/Priamokut.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+
+public class Priamokut
+{
+    public int a;
+    public int b;
+    public string name;
+    public int x;
+    public int y;
+    public int SideA
+    {
+        get { return a; }
+        set { a = value; }
+    }
+    public int SideB
+    {
+        get { return b; }
+        set { b = value; }
+    }
+    public int CoordinateX
+    {
+        get { return x; }
+        set { x = value; }
+    }
+    public int CoordinateY
+    {
+        get { return y; }
+        set { y = value; }
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public void Generate(int a, int b, string name)
+    {
+        Console.WriteLine($"Створено прямокутник з ім'ям {name}, стороною а={a}см та стороною b={b}см");
+    }
+    public void Coordinete(int x, int y)
+    {
+        Console.WriteLine($"Координати лівого верхнього кута ({x},{y})");
+    }
+    public void Move(int x, int y)
+    {
+        Console.WriteLine("Переміщення вліво(зі знаком мінус), переміщення вправо(без знака мінус)");
+        int dx = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Переміщення вниз(зі знаком мінус), переміщення вгору(без знака мінус)");
+        int dy = Convert.ToInt32(Console.ReadLine());
+        int x1 = x + dx;
+        int y1 = y + dy;
+        Console.WriteLine($"Тепер координати верхнього лівого кута ({x1},{y1})");
+    }
+    public void Resize(int a, int b)
+    {
+        Console.WriteLine("Зменшення висоти(зі знаком мінус), збільшення висоти(без знака мінус)");
+        int a1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Зменшення ширини(зі знаком мінус), збільшення ширини(без знака мінус)");
+        int b1 = Convert.ToInt32(Console.ReadLine());
+        int a_Resize = a + a1;
+        int b_Resize = b + b1;
+        Console.WriteLine($"Змінена висота прямокутника - {a_Resize}. Змінена ширина прямокутника - {b_Resize}");
+    }
+    public void Union(int x, int y, int a, int b, int x1, int y1, int a1, int b1)
+    {
+        int minX = Math.Min(x, x1);
+        int minY = Math.Min(y, y1);
+        int maxX = Math.Max(y + b, y1 + b1);
+        int maxY = Math.Max(x + a, x1 + a1);
+        int width = maxX - minX;
+        int height = maxY - minY;
+        Console.WriteLine("Найменший прямокутник, що охоплює обидва задані прямокутники:");
+        Console.WriteLine($"Верхній лівий кут: ({minX}, {minY})");
+        Console.WriteLine($"Ширина: {width}");
+        Console.WriteLine($"Висота: {height}");
+    }
+    public void Intersect(int x, int y, int a, int b, int x1, int y1, int a1, int b1)
+    {
+        int x_min = Math.Max(x, x1);
+        int y_min = Math.Max(y, y1);
+        int x_max = Math.Min(x + b, x1 + b1);
+        int y_max = Math.Min(y + a, y1 + a1);
+        if (x_min >= x_max || y_min >= y_max)
+        {
+            Console.WriteLine("Прямокутники не перетинаються");
+        }
+        else
+        {
+            int h = y_max - y_min;
+            int w = x_max - x_min;
+            Console.WriteLine($"Координати верхнього лівого кута прямокутника-перетину: ({x_min},{y_min})");
+            Console.WriteLine($"Розміри прямокутника-перетину: ширина - {w}, висота - {h}");
+        }
+    }
+}
---
+++ 3LL/Program.cs
@@ -0,0 +1,54 @@
+﻿//Скласти опис класу прямокутників зі сторонами, паралельними осям координат.Передбачити можливість переміщення прямокутників на площині, зміна розмірів,
+//побудова найменшого прямокутника, що містить два заданих прямокутники, і прямокутника,що є спільною частиною (перетином) двох прямокутників.
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Priamokut priam = new Priamokut();
+        Console.WriteLine("Введіть висоту  прямокутника:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть ширину  прямокутника:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть назву прямокутника:");
+        string name = Convert.ToString(Console.ReadLine());
+        priam.b = b;
+        priam.a = a;
+        priam.Name = name;
+        Console.WriteLine("Введіть координату х верхнього лівого кута прямокутника:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть координату у верхнього лівого кута прямокутника:");
+        int y = Convert.ToInt32(Console.ReadLine());
+        priam.x = x;
+        priam.y = y;
+        priam.Generate(a, b, name);
+        priam.Coordinete(x, y);
+        Priamokut priam1 = new Priamokut();
+        Console.WriteLine("Введіть висоту 2 прямокутника:");
+        int a1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть ширину 2 прямокутника:");
+        int b1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть назву 2 прямокутника:");
+        string name1 = Convert.ToString(Console.ReadLine());
+        priam1.b = b1;
+        priam1.a = a1;
+        priam1.Name = name1;
+        Console.WriteLine("Введіть координату х верхнього лівого кута 2 прямокутника:");
+        int x1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть координату у верхнього лівого кута 2 прямокутника:");
+        int y1 = Convert.ToInt32(Console.ReadLine());
+        priam1.x = x1;
+        priam1.y = y1;
+        priam1.Generate(a1, b1, name1);
+        priam1.Coordinete(x1, y1);
+        priam.Union(x, y, a, b, x1, y1, a1, b1);
+        priam.Intersect(x, y, a, b, x1, y1, a1, b1);
+        priam.Move(x, y);
+        priam.Resize(a, b);
+        using (FileStream fs = new FileStream("C:\\Users\\KEY\\Downloads\\LL3.json", FileMode.OpenOrCreate))
+        {
+            JsonSerializer.SerializeAsync<Priamokut>(fs, priam);
+        }
+    }
+}
